d89782f66643cd7d08d398b69669d4af4287145d
Natti94 <natnael.berhane.sv@gmail.com>
2025-05-07 18:52:36 +0200

    update

--- quiz-app/vite.config.js
+++ quiz-app/vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: "/Quiz_Agil_Projektmetodik_Testning/",
   plugins: [react()],
-  base: "/quiz-app/",
 });
