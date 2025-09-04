3d5932e40c8165636d4e1060685823810034162e
Natti94 <natnael.berhane.sv@gmail.com>
2025-09-04 13:47:35 +0200

    making ready for deployment to netlify

--- quiz-app/vite.config.js
+++ quiz-app/vite.config.js
@@ -2,6 +2,5 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/Quiz_Agil_Projektmetodik_Testning/",
   plugins: [react()],
 });
